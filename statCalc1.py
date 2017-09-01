639b43781b4279f61e69d8fa0ee61f5581b4a710
AustinV9121 <srv114@utulsa.edu>
2017-08-31 21:43:29 -0500

    AustVStatCalc
    standard functionality upgrade, fixed table oversizing on multiple selections, new file explorer 'choose file' option

--- statCalc1.py
+++ statCalc1.py
@@ -15,14 +15,7 @@ from PyQt5.QtWidgets import (QWidget, QTreeView, QMessageBox, QHBoxLayout,
                              QLineEdit, QVBoxLayout, QApplication, QPushButton,
                              QTableWidget, QTableWidgetItem)
 from PyQt5.QtCore import Qt, QTimer, QCoreApplication
-from PyQt5 import QtGui, QtCore, QtWidgets
-from tkinter import filedialog
-from tkinter import *
-import subprocess
-import tkinter
 
-from scipy import stats
-import statistics
 import numpy as np #np is a pointer to the numpy library, hence np.fn() calls a numpy subfunction
 
 class StatCalculator(QWidget):
@@ -32,50 +25,26 @@ class StatCalculator(QWidget):
 
         # Upon startup, run a user interface routine
         self.init_ui()
-        
+              
+
     def init_ui(self):
-        
         #Builds GUI
         self.setGeometry(200,200,500,500)
-#==============================================================================
-#         
-#         #menu bar
-#         self.menu_bar=QtWidgets.QMenuBar(self)#QtWidgets.QMenuBar.addMenu(topmenu, 'Open')
-#         # file menu
-#         self.menu_file = QtWidgets.QMenu(self.menu_bar)
-#         self.menu_file.setTitle('File')
-#         self.menu_bar.addAction(self.menu_file.menuAction())
-# 
-#         # exit action
-#         self.menu_action_exit = QtWidgets.QAction(self)
-#         self.menu_action_exit.setText("Exit")
-#         self.menu_action_exit.triggered.connect(self.close)
-#==============================================================================
 
         b1 = QWidget()
         self.load_button = QPushButton(b1)
         self.load_button.setText('Load Data')
-        self.load_button.clicked.connect(self.simp_load)
+        self.load_button.clicked.connect(self.load_data)
         
         b2 = QWidget()
-        self.stats_button = QPushButton(b2)
+        self.stats_button = QPushButton(b1)
         self.stats_button.setText('Compute Statistics')
         self.stats_button.clicked.connect(self.compute_stats)
         
-        b3=QWidget()
-        self.file_button=QPushButton(b3, text="Choose File")
-        self.file_button.clicked.connect(self.choose_file)#
-        
         self.mean_label = QLabel("Mean: Not Computed Yet",self)
         self.median_label = QLabel("Median: Not Computed Yet",self)
-        self.mode_label=QLabel("Mode: Not Computed Yet", self)
         self.stdDev_label = QLabel("Standard Deviation: Not Computed Yet",self)
-        self.kurt_label=QLabel("Kurtosis: Not Computed Yet",self)
-        self.skew_label=QLabel("Skew: Not Computed Yet",self)
-        self.minVal_label=QLabel("Minimum: Not Computed Yet",self)
-        self.maxVal_label=QLabel("Maximum: Not Computed Yet",self)
         self.range_label = QLabel("Range: Not Computed Yet",self)
-        self.sumT_label=QLabel("Sum: Not Computed Yet",self)
         
         #Set up a Table to display data
         self.data_table = QTableWidget()
@@ -84,21 +53,13 @@ class StatCalculator(QWidget):
         #Define where the widgets go in the window        
         v_layout = QVBoxLayout()
         
-        #v_layout.addWidget(self.menu_bar)
         v_layout.addWidget(self.load_button)
-        #v_layout.addWidget(self.stats_button)
-        v_layout.addWidget(self.file_button)
+        v_layout.addWidget(self.stats_button)
         v_layout.addWidget(self.data_table)
         v_layout.addWidget(self.mean_label)
         v_layout.addWidget(self.median_label)
-        v_layout.addWidget(self.mode_label)
         v_layout.addWidget(self.stdDev_label)
-        v_layout.addWidget(self.kurt_label)
-        v_layout.addWidget(self.skew_label)
-        v_layout.addWidget(self.minVal_label)
-        v_layout.addWidget(self.maxVal_label)
         v_layout.addWidget(self.range_label)
-        v_layout.addWidget(self.sumT_label)
         
         self.setLayout(v_layout)
         self.setWindowTitle('AustinVs Calc: Introduction to Descriptive Statistics')
@@ -106,16 +67,9 @@ class StatCalculator(QWidget):
         self.raise_()
         self.show()
     
-    def simp_load(self):
-        self.load_data("Historical Temperatures from Moose Wyoming.csv")
-        
-    def load_data(self, filename):    
-       try:
-           self.data_table.setRowCount(0)
-       except:
-           pass
+    def load_data(self):        
        #for this example, we'll hard code the file name.
-       data_file_name = filename
+       data_file_name = "Historical Temperatures from Moose Wyoming.csv"
        header_row = 1 
        #load data file into memory as a list of lines       
        with open(data_file_name,'r') as data_file:
@@ -123,7 +77,7 @@ class StatCalculator(QWidget):
         
        print("Opened {}".format(data_file_name))
        print(self.data_lines[1:10])
-
+        
        #Set the headers
        #parse the lines by stripping the newline character off the end
        #and then splitting them on commas.
@@ -142,15 +96,6 @@ class StatCalculator(QWidget):
                entry = QTableWidgetItem("{}".format(row_values[col]))
                self.data_table.setItem(current_row,col,entry)
        print("Filled {} rows.".format(row))
-        
-    def choose_file(self):
-        root = Tk()
-        root.wantedfile =  filedialog.askopenfilename(initialdir = "C:\\" ,title = "Select file",filetypes = (("tab delineated",".csv"),("all files","*.*")))
-        #subprocess.Popen(r'explorer \select, "C:"')
-        try:
-            self.load_data(root.wantedfile)
-        except:
-            pass
     
     def compute_stats(self):
         
@@ -162,47 +107,20 @@ class StatCalculator(QWidget):
                 item_list.append(float(item.text()))
             except:
                 pass
-        #print(item_list)
-        #print(items)
-    
         data_array = np.asarray(item_list)
         mean_value = np.mean(data_array)
         medianVal=np.median(data_array)
-        sumT=sum(item_list)
-        length=len(item_list)
+        stdDev=np.std(data_array)
         minVal=np.amin(data_array)
         maxVal=np.amax(data_array)
         rangeVal=maxVal-minVal
-        stdDev=np.std(data_array)
-        kurt=stats.kurtosis(data_array)
- #block comment by ctrl+4, uncomment ctrl+1
-        skew=stats.skew(data_array)
-        try:
-            modeVal=statistics.mode(data_array)
-        except:
-            modeVal=0
-            pass
-                
-        if len(item_list)>2:
-            varVal=statistics.variance(item_list)
-        else:
-            varVal=0
         #first number ,n, in {n:pf} is index of call in format()
-        print("Mean = {0:8.6f} \tMedian= {1:8.6f} \tMode{3:4.1f}\tStandardDeviation= {2:8.6f}".format(mean_value, medianVal, stdDev, modeVal))
-        print("  Kurtosis= {0:8.6f}\tSkew= {1:8.6f}\tVariance= {2:8.6f}".format(kurt, skew, varVal))
-        print("  MinVal= {0:5.1f}\tMaxVal= {1:5.1f} \tRange= {2:5.1f}".format(minVal, maxVal, rangeVal))
-        print("  Items Selected:{0:5.0f} with total sum of values:{1:5.0f} ".format(length, sumT))
-        
-        self.mean_label.setText("Mean = {:0.6f}".format(mean_value))
-        self.median_label.setText("Median = {:0.6f}".format(medianVal))
-        self.mode_label.setText("Mode = {:0.6f}".format(modeVal))
-        self.stdDev_label.setText("StdDev = {:0.6f}".format(stdDev))
-        self.kurt_label.setText("Kurtosis= {:0.6f}".format(kurt))
-        self.skew_label.setText("Skew= {:0.6f}".format(skew))
-        self.minVal_label.setText("Minimum= {:0.1f}".format(minVal))
-        self.maxVal_label.setText("Maximum= {:0.1f}".format(maxVal))
-        self.range_label.setText("Range = {:0.1f}".format(rangeVal))
-        self.sumT_label.setText("Sum= {:0.1f}".format(sumT))
+        print("Mean = {0:5f} \tMedian={1:5f} \tStandardDeviation={2:5f}".format(mean_value, medianVal, stdDev))
+        print("\tMinVal={0:5f} \tMaxVal={1:5f} \tRange={2:5f}".format(minVal, maxVal, rangeVal))
+        self.mean_label.setText("Mean = {:0.3f}".format(mean_value))
+        self.median_label.setText("Median = {:0.3f}".format(medianVal))
+        self.stdDev_label.setText("StdDev = {:0.3f}".format(stdDev))
+        self.range_label.setText("Range = {:0.3f}".format(rangeVal))
 '''       
 Assignment: 
 1. Add all the quantities from the MS Excel descriptive Statistics 
