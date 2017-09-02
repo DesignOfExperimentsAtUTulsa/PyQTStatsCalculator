7b20c9c67de6f148f0f6915fc054c98d61b1c6ed
justiceb49 <31358249+justiceb49@users.noreply.github.com>
2017-09-01 21:43:31 -0500

    Add files via upload

--- 01_basic_GUI_with_table.py
+++ 01_basic_GUI_with_table.py
@@ -6,10 +6,16 @@ import sys
 from PyQt5.QtWidgets import (QWidget, QTreeView, QMessageBox, QHBoxLayout, 
                              QFileDialog, QLabel, QSlider, QCheckBox, 
                              QLineEdit, QVBoxLayout, QApplication, QPushButton,
-                             QTableWidget, QTableWidgetItem)
+                             QTableWidget, QTableWidgetItem, QInputDialog, QLineEdit)
 from PyQt5.QtCore import Qt, QTimer, QCoreApplication
 
 import numpy as np
+
+import scipy.stats
+
+import statistics
+
+from PyQt5.QtGui import QIcon
 
 class StatCalculator(QWidget):
 
@@ -23,14 +29,21 @@ class StatCalculator(QWidget):
     def init_ui(self):
         #Builds GUI
         self.setGeometry(200,200,500,500)
+        
+        
 
         b1 = QWidget()
-        self.load_button = QPushButton(b1)
+        self.get_button = QPushButton(b1)
+        self.get_button.setText('set file address')
+        self.get_button.clicked.connect(self.get_address)
+        
+        b2 = QWidget()
+        self.load_button = QPushButton(b2)
         self.load_button.setText('Load Data')
         self.load_button.clicked.connect(self.load_data)
         
-        b2 = QWidget()
-        self.stats_button = QPushButton(b1)
+        b3 = QWidget()
+        self.stats_button = QPushButton(b3)
         self.stats_button.setText('Compute Statistics')
         self.stats_button.clicked.connect(self.compute_stats)
         
@@ -43,20 +56,30 @@ class StatCalculator(QWidget):
         #Define where the widgets go in the window        
         v_layout = QVBoxLayout()
         
+        v_layout.addWidget(self.get_button)
         v_layout.addWidget(self.load_button)
         v_layout.addWidget(self.stats_button)
         v_layout.addWidget(self.data_table)
         v_layout.addWidget(self.mean_label)
         
         self.setLayout(v_layout)
-        self.setWindowTitle('Introduction to Descriptive Statistics')
+        self.setWindowTitle('Introduction to Descriptive Statistics-Justice Boisselle')
         self.activateWindow()
         self.raise_()
         self.show()
     
-    def load_data(self):        
-       #for this example, we'll hard code the file name.
-       data_file_name = "Historical Temperatures from Moose Wyoming.csv"
+    def get_address(self):
+        address, okPressed = QInputDialog.getText(self, "data","address of data file", QLineEdit.Normal, "")
+        if okPressed and address != '':
+            print(address)
+        global file_address 
+        file_address = address
+    
+    
+    def load_data(self):   
+
+       
+       data_file_name = file_address
        header_row = 1 
        #load data file into memory as a list of lines       
        with open(data_file_name,'r') as data_file:
@@ -95,9 +118,46 @@ class StatCalculator(QWidget):
             except:
                 pass
         data_array = np.asarray(item_list)
+
         mean_value = np.mean(data_array)
+        standard_error = np.std(data_array)
+        median= np.median(data_array)
+        mode = statistics.mode(item_list)
+        standard_div = np.std(data_array)
+        variance = np.var(data_array)
+        kurtosis = scipy.stats.kurtosis(data_array)
+        skew = scipy.stats.skew(data_array)
+        data_range = np.ptp(data_array)
+        minimum = np.amin(data_array)
+        maximum = np.amax(data_array)
+        summation = np.sum(data_array)
+        count = len(data_array)
+        large = maximum
+        small = minimum
+        confidence = scipy.stats.t.interval(0.95, count, loc = 1, scale = 1)
+#        
         print("Mean = {0:5f}".format(mean_value))
+        print("Standard Error = {0:5f}".format(standard_error))
+        print("Median = {0:5f}".format(median))
+        print("Mode = {0:5f}".format(mode))
+        print("Standard Deviation = {0:5f}".format(standard_div))
+        print("Variance = {0:5f}".format(variance))
+        print("Kurtosis = {0:5f}".format(kurtosis))
+        print("Skewness = {0:5f}".format(skew))
+        print("Range = {0:5f}".format(data_range))
+        print("Minimum = {0:5f}".format(minimum))
+        print("Maximum = {0:5f}".format(maximum))
+        print("Sum = {0:5f}".format(summation))
+        print("Count = {0:5f}".format(count))
+        print("Largest = {0:5f}".format(large))
+        print("Smallest = {0:5f}".format(small))
+        print("Confidence = {0:5f}".format(confidence))
+
+
         self.mean_label.setText("Mean = {:0.3f}".format(mean_value))
+        
+    
+            
 '''       
 Assignment: 
 1. Add all the quantities from the MS Excel descriptive Statistics 
